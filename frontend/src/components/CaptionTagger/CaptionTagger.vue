efda75a6e4b5e47a00afb3d3aa188c9ae0c14f81
Wesley Castro <root670@gmail.com>
2020-02-22 15:28:44 -0800

    Highlight individual words.

--- frontend/src/components/CaptionTagger/CaptionTagger.vue
+++ frontend/src/components/CaptionTagger/CaptionTagger.vue
@@ -1,51 +1,56 @@
 <template>
 <v-container>
   <v-row>
-
-  <v-col cols="12" sm="6">
-    <v-text-field
-      v-model="videoUrl"
-      label="Video URL"
-      single-line
-      @keydown.enter="fetchCaption"
-    ></v-text-field>
-    <v-btn
-      color="primary"
-      :loading="loading"
-      :disabled="loading"
-      @click="fetchCaption"
-    >
-      Fetch
-    </v-btn>
-  </v-col>
-
+    <v-col cols="12" sm="6">
+      <v-text-field
+        v-model="videoUrl"
+        label="Video URL"
+        single-line
+        @keydown.enter="fetchCaption"
+      ></v-text-field>
+      <v-btn
+        color="primary"
+        :loading="loading"
+        :disabled="loading"
+        @click="fetchCaption"
+      >
+        Fetch
+      </v-btn>
+    </v-col>
   </v-row>
-  <v-row>
-    <Captions
-      :data="captions"
-      ></Captions>
-  </v-row>
+  <v-container fluid>
+    <v-row dense ref="selectable">
+      <span
+        v-for="(caption, idx) in captions"
+        :key="idx"
+        :id="idx"
+        class="word lighten-4"
+        :class="{
+          purple: caption.tag == DEFAULT_TEXT_LABEL,
+          green: caption.tag == TEXT_LABELS.SPONSOR_MENTION
+        }"
+      >{{ caption.word }}</span>
+    </v-row>
+  </v-container>
 </v-container>
 </template>
 
 <script>
 
 import axios from 'axios';
-import { API_URL } from '@/constants.js';
-import Captions from './Captions';
+import { API_URL, DEFAULT_TEXT_LABEL, TEXT_LABELS } from '@/constants.js';
 
 const VIDEO_ID_REGEX = "^.*(?:youtu.be/|v/|e/|u/w+/|embed/|v=)([^#&?]*).*";
 
 export default {
     name: 'CaptionTagger',
-    components: {
-      Captions
-    },
     data: function() {
       return {
         videoUrl: '',
         captions: [],
         loading: false,
+        DEFAULT_TEXT_LABEL: DEFAULT_TEXT_LABEL,
+        TEXT_LABELS: TEXT_LABELS
       }
     },
     computed: {
@@ -54,7 +59,26 @@ export default {
         return (match == null) ? '' : match[1];
       }
     },
+    mounted () {
+    window.addEventListener('mouseup', this.onMouseup)
+    },
+
+    beforeDestroy () {
+      window.removeEventListener('mouseup', this.onMouseup)
+    },
     methods: {
+      onMouseup() {
+        console.log('Mouse up');
+        const selection = window.getSelection();
+        const startNode = selection.getRangeAt(0).startContainer.parentNode;
+        const endNode = selection.getRangeAt(0).endContainer.parentNode;
+        if(startNode.parentNode == this.$refs["selectable"])
+        {
+          console.log('start:' + startNode.id);
+          console.log('end:' + endNode.id);
+          selection.removeAllRanges();
+        }
+      },
       fetchCaption() {
         if (this.videoId == '')
           return;
@@ -64,7 +88,9 @@ export default {
 
         axios.get(API_URL + '/' + this.videoId)
           .then((response) => {
-            this.captions = response.data;
+            this.captions = response.data.map(obj => ({
+              ...obj, tag: DEFAULT_TEXT_LABEL
+            }));
           })
           .catch((error) => {
             console.log(error)
@@ -78,5 +104,7 @@ export default {
 </script>
 
 <style scoped>
-
+.word {
+  margin-right: 1em;
+}
 </style>
--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,1 +1,9 @@
 export const API_URL = 'http://127.0.0.1:5000';
+
+export const TEXT_LABELS = {
+    OTHER: 'Other',
+    SPONSOR_MENTION: 'Sponsor Mention',
+    INTRO: 'Intro',
+    OUTRO: 'Outro/Request for Sub.',
+};
+export const DEFAULT_TEXT_LABEL = TEXT_LABELS.OTHER;
